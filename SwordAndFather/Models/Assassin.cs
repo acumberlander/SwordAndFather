956f0b9d6a7c969716bbd7a9335319207abb3c18
Austin <austincumberlander@gmail.com>
2019-03-30 13:07:15 -0500

    got up to date

---
+++ SwordAndFather/Models/Assassin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SwordAndFather.Models
+{
+    public class Assassin
+    {
+        public int Id { get; set; }
+        public string CodeName { get; set; }
+        public string Catchphrase { get; set; }
+        public string PreferredWeapon { get; set; }
+        public int Rating { get; set; }
+        public decimal StandardFee { get; set; }
+
+        public Assassin(string codeName, string catchphrase, string preferredWeapon)
+        {
+            CodeName = codeName;
+            Catchphrase = catchphrase;
+            PreferredWeapon = preferredWeapon;
+        }
+    }
+}
---
+++ SwordAndFather/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SwordAndFather.Models
+{
+    public class User
+    {
+        //Properties
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        //Constructor
+        public User(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+    }
+}
